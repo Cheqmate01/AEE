b6cddba9cb6b993f1eaacfde0752cedd30bab704
Cheqmate01 <cheqmater01@gmail.com>
2025-07-02 11:49:55 +0100

    Responsive Mobile Fix

--- src/components/AboutUsSec.vue
+++ src/components/AboutUsSec.vue
@@ -1,24 +1,26 @@
 <template>
-  <section class="bg-yellow-500 mx-3 sm:ml-10 py-10 space-b-10">
-    <h1 class="langar-regular text-3xl sm:text-7xl font-black mb-4">ABOUT US</h1>
-    <div class="sm:flex sm:flex-row justify-between gap-x-4">
+  <section class="px-3 sm:pl-10 py-10 space-b-10 sm:flex sm:flex-row justify-between gap-x-4">
+    <div class="bg-yellow-500">
+      <h1 class="langar-regular text-3xl sm:text-7xl font-black mb-4">ABOUT US</h1>
       <div class="">
-        <div class=" pb-20">
-          <p class="leading-tight">
-            The premise of Ajayi Elebire Elite was founded on the memory and life of Olalekan Ajayi's father, Babatunde Ajayi, a.k.a Elebire.
-          </p>
-          <p class="leading-tight mt-4">
-            Our mission is to mold young athletes into quality players and human beings, instilling the significance of fundamentals on and off the court as the stomping ground.
-          </p>
-          <button>
-            <a class="flex flex-row mt-10 langar-regular text-xl sm:text-2xl hover:text-gray-800 gap-hover items-start" href="/about-us">
-              <p class="transition-colors duration-300">LEARN MORE</p>
-              <img src="/right-arrow-black.png" width="32" alt="">
-            </a>
-          </button>
+        <div class="">
+          <div class=" pb-20">
+            <p class="leading-tight">
+              The premise of Ajayi Elebire Elite was founded on the memory and life of Olalekan Ajayi's father, Babatunde Ajayi, a.k.a Elebire.
+            </p>
+            <p class="leading-tight mt-4">
+              Our mission is to mold young athletes into quality players and human beings, instilling the significance of fundamentals on and off the court as the stomping ground.
+            </p>
+            <button>
+              <a class="flex flex-row mt-10 langar-regular text-xl sm:text-2xl hover:text-gray-800 gap-hover items-start" href="/about-us">
+                <p class="transition-colors duration-300">LEARN MORE</p>
+                <img src="/right-arrow-black.png" width="32" alt="">
+              </a>
+            </button>
+          </div>
         </div>
       </div>
-      <img class="w-full sm:w-3/5 overflow-y-visible" src="/picture1.jpg" alt="">
     </div>
+    <img class="w-full sm:w-3/5 overflow-y-visible" src="/picture1.jpg" alt="">
   </section>
 </template>
