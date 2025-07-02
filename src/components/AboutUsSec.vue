33c9b044fae3611dede3edb485b04d77e0648164
Cheqmate01 <cheqmater01@gmail.com>
2025-07-02 10:39:05 +0100

    Responsive Mobile Fix

--- src/components/AboutUsSec.vue
+++ src/components/AboutUsSec.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="bg-yellow-500">
-    <div class="sm:ml-10 py-10 space-b-10 pb-20">
+    <div class="mx-3 sm:ml-10 py-10 space-b-10 pb-20">
       <h1 class="langar-regular text-3xl sm:text-7xl font-black mb-4">ABOUT US</h1>
       <div class="flex flex-row justify-between gap-x-4">
         <div>
@@ -11,13 +11,13 @@
             Our mission is to mold young athletes into quality players and human beings, instilling the significance of fundamentals on and off the court as the stomping ground.
           </p>
           <button>
-            <a class="flex flex-row mt-10 langar-regular text-xl sm:text-2xl hover:text-gray-800 gap-hove items-start" href="/about-us">
+            <a class="sm:flex sm:flex-row mt-10 langar-regular text-xl sm:text-2xl hover:text-gray-800 gap-hove items-start" href="/about-us">
               <p class="transition-colors duration-300">LEARN MORE</p>
               <img src="/right-arrow-black.png" width="32" alt="">
             </a>
           </button>
         </div>
-        <img class="w-3/5 overflow-y-visible" src="/Picture1.png" alt="">
+        <img class="w-3/5 overflow-y-visible" src="/picture1.png" alt="">
       </div>
     </div>
   </section>
