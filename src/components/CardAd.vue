8e78af15e01c3230c74860351d453c816be16ba0
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:52:44 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="bg-yellow-500 text-black">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl py-10 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
+      <a class="langar-regular text-3xl my-10 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
         Community Engagement
         <i class="fi fi-br-arrow-small-right text-black"></i>
       </a>
