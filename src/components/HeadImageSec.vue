8bbe07706ad3230c85a642b27fc2f7e83efe3cac
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 12:56:39 +0100

    owner choice edits

--- src/components/HeadImageSec.vue
+++ src/components/HeadImageSec.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="-mt-28">
-    <img class="block w-full h-dvh object-cover" src="/head-image.jpg" alt="">
+  <div class="-mt-28 h-dvh">
+    <img class="block w-full h-full object-cover" src="/head-image.jpg" alt="">
   </div>
 </template>
