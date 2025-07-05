fb614f7c9f77441263b53e3040990dab11fa3ee7
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:33:10 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="bg-black text-yellow-500">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl">
+      <a class="langar-regular text-3xl py-4 ml-4 hover:text-yellow-300 active:text-yellow-300 transition-colors duration-300" href="/past-events">
         Past Events
         <i class="fi fi-br-arrow-small-right my-auto"></i>
       </a>
