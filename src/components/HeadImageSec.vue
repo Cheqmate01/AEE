302eee4f4ed8493c0de2a79bacd9b531fcc899ba
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 11:58:33 +0100

    owner choice edits

---
+++ src/components/HeadImageSec.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-[url(/headimage.jpg)] w-full h-dvh">
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import AboutUs from '@/components/AboutUsSec.vue';
 import BallAd from '@/components/BallAd.vue';
 import CollageAd from '@/components/CollageAd.vue';
+import HeadImage from '@/components/HeadImageSec.vue';
 import HypeAd from '@/components/HypeAd.vue';
 import UpdateSec from '@/components/UpdateSec.vue';
 </script>
 
 <template>
+  <HeadImage />
   <BallAd />
   <CollageAd />
   <AboutUs />
