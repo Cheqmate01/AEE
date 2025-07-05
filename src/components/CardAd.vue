59a17759eea9584525070142fbd80c5b64dffaa9
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 16:01:47 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="space-y-10">
-    <div class="bg-black text-yellow-500">
-      <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl my-50 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
+  <div class="space-y-10 mb-20 sm:mb-32">
+    <div class="bg-black text-yellow-500 pb-5">
+      <img class="w-full mb-5" src="/head-image.jpg" alt="">
+      <a class="langar-regular text-3xl ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
         Past Events
         <i class="fi fi-br-arrow-small-right my-auto"></i>
       </a>
     </div>
-    <div class="bg-yellow-500 text-black">
-      <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl my-50 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
+    <div class="bg-yellow-500 text-black pb-5">
+      <img class="w-full mb-5" src="/head-image.jpg" alt="">
+      <a class="langar-regular text-3xl ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
         Community Engagement
         <i class="fi fi-br-arrow-small-right text-black"></i>
       </a>
