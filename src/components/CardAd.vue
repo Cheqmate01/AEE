4d715532300051aac3653b8c5398c0610ad688d8
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:54:47 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -2,14 +2,14 @@
   <div class="space-y-10">
     <div class="bg-black text-yellow-500">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl my-10 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
+      <a class="langar-regular text-3xl my-20 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
         Past Events
         <i class="fi fi-br-arrow-small-right my-auto"></i>
       </a>
     </div>
     <div class="bg-yellow-500 text-black">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl my-10 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
+      <a class="langar-regular text-3xl my-20 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
         Community Engagement
         <i class="fi fi-br-arrow-small-right text-black"></i>
       </a>
