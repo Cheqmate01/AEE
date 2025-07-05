d50efcd70ad30131e3701c559dfa695f4996739f
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:48:51 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -2,13 +2,17 @@
   <div>
     <div class="bg-black text-yellow-500">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl py-4 ml-4 hover:text-yellow-300 active:text-yellow-300 transition-colors duration-300" href="/past-events">
+      <a class="langar-regular text-3xl py-10 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
         Past Events
         <i class="fi fi-br-arrow-small-right my-auto"></i>
       </a>
     </div>
-    <div>
-
+    <div class="bg-yellow-500 text-black">
+      <img class="w-full" src="/head-image.jpg" alt="">
+      <a class="langar-regular text-3xl py-10 ml-4 hover:text-gray-800 gap-hover active:text-yellow-300 transition-colors duration-300" href="/community-engagement">
+        Community Engagement
+        <i class="fi fi-br-arrow-small-right text-black"></i>
+      </a>
     </div>
   </div>
 </template>
