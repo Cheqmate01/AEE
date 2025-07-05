300f1fd44da55196bf5c23055043781a967cba42
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:53:14 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -2,7 +2,7 @@
   <div class="space-y-10">
     <div class="bg-black text-yellow-500">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a class="langar-regular text-3xl py-10 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
+      <a class="langar-regular text-3xl my-10 ml-4 hover:text-yellow-300 gap-hover active:text-yellow-300 transition-colors duration-300" href="/past-events">
         Past Events
         <i class="fi fi-br-arrow-small-right my-auto"></i>
       </a>
