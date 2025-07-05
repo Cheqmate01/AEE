46957100dcce5b55aecbb6944bb15574ed20de35
Cheqmate01 <cheqmater01@gmail.com>
2025-07-05 15:04:22 +0100

    owner choice edits

--- src/components/CardAd.vue
+++ src/components/CardAd.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div>
+    <div class="bg-black text-yellow-500">
       <img class="w-full" src="/head-image.jpg" alt="">
-      <a>
+      <a class="langar-regular text-xl">
         Past Events
-        <i class="fi fi-br-arrow-small-right self-baseline text-black"></i>
+        <i class="fi fi-br-arrow-small-right self-baseline text-black my-auto"></i>
       </a>
     </div>
     <div>
