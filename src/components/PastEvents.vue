7d20be5bef07941ae0fce8270139754356b2e30c
Cheqmate01 <cheqmater01@gmail.com>
2025-07-06 16:30:32 +0100

    owner choice edits

--- src/components/PastEvents.vue
+++ src/components/PastEvents.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-xl mx-auto my-10 p-6">
-    <h1 class="langar-regular text-3xl font-bold mb-4 text-yellow-500">Previous Camp Dates</h1>
-    <ul class="list-disc pl-6 space-y-2 text-lg text-gray-800 float-right">
+    <h1 class="langar-regular text-3xl mb-4 text-yellow-500">Previous Camp Dates</h1>
+    <ul class="list-disc pl-6 space-y-2 text-lg text-black mb-20">
       <li>July, 2020</li>
       <p>Rehabilitation Center Isheri Lagos State</p>
       <li>July, 2021</li>
@@ -13,6 +13,5 @@
       <li>July, 2024</li>
       <p>Ilupeju Basketball Court</p>
     </ul>
-    <span class="mt-20"></span>
   </div>
 </template>
