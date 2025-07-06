82ce2192876e9adbef51af367ac695af65e474cd
Cheqmate01 <cheqmater01@gmail.com>
2025-07-06 03:12:42 +0100

    owner choice edits

--- src/components/CommunityEngagement.vue
+++ src/components/CommunityEngagement.vue
@@ -2,7 +2,7 @@
   <div class="-mt-28 mb-10">
     <div class="relative h-screen">
       <video class="block h-full object-cover" src="/charity-vid-1.MP4" autoplay muted loop></video>
-      <div class="absolute flex items-end w-full h-screen bg-transparent">
+      <div class="absolute inset-0 flex items-end bg-transparent">
         <h1 class="langar-regular ml-10 mb-10 text-yellow-500 text-5xl">CHARITY COMMUNITY</h1>
       </div>
     </div>
