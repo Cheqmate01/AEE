0c7c1cfac9909384f5fc8ce919e8c3aadb9870b0
Cheqmate01 <cheqmater01@gmail.com>
2025-07-06 02:21:53 +0100

    owner choice edits

--- src/components/CommunityEngagement.vue
+++ src/components/CommunityEngagement.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="-mt-28 mb-10 h-screen">
+  <div class="-mt-28 mb-10">
     <div class="absolute flex items-end w-full h-screen bg-transparent">
       <h1 class="langar-regular ml-10 mb-10 text-yellow-500 text-5xl">CHARITY COMMUNITY</h1>
     </div>
