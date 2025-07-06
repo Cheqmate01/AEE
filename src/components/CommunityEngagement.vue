db42037d4a58763837801501cffe48cbe6bc0f37
Cheqmate01 <cheqmater01@gmail.com>
2025-07-06 02:53:26 +0100

    owner choice edits

--- src/components/CommunityEngagement.vue
+++ src/components/CommunityEngagement.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="-mt-28 mb-10">
-    <div>
+    <div class="relative h-screen">
+      <video class="block h-full object-cover" src="/charity-vid-1.MP4" autoplay muted loop></video>
       <div class="absolute flex items-end w-full h-screen bg-transparent">
         <h1 class="langar-regular ml-10 mb-10 text-yellow-500 text-5xl">CHARITY COMMUNITY</h1>
       </div>
-      <video class="block h-full object-cover" src="/charity-vid-1.MP4" autoplay muted loop></video>
     </div>
-    <div class="mt-screen">
+    <div class="mx-10 my-10 space-y-10">
       <img src="/charity-pic-1.PNG" alt="">
-      <p>We are dedicated to providing support and resources to vulnerable communities, promoting education, health, economic & youth empowerments and promoting community development and social welfare.</p>
-      <video class="block w-full object-cover" src="/charity-vid-2.MP4" autoplay muted loop></video>
+      <div class="sm:grid sm:grid-cols-2 items-start gap-6">
+        <p>We are dedicated to providing support and resources to vulnerable communities, promoting education, health, economic & youth empowerments and promoting community development and social welfare.</p>
+        <video class="block w-full object-cover" src="/charity-vid-2.MP4" autoplay muted loop></video>
+      </div>
     </div>
   </div>
 </template>
--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -38,8 +38,8 @@
         </li>
         <li>
           <a title="Follow on TikTok" href="https://www.tiktok.com/@ajayielebireelite?_t=ZM-8xiFZAx6S0L&_r=1" target="_blank">
-            <div class="hover:text-yellow-500 active:text-yellow-300 text-white transition-colors duration-300 w-36 h-36">
-              <i class="fa-brands fa-tiktok"></i>
+            <div class="w-36 h-36">
+              <i class="fa-brands fa-tiktok hover:text-yellow-500 active:text-yellow-300 text-white transition-colors duration-300"></i>
             </div>            
           </a>
         </li>
