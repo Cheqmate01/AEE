228c95380eb5c3dcde1c37cb58d7c8a6bb8b2228
Cheqmate01 <cheqmater01@gmail.com>
2025-07-08 01:29:10 +0100

    CE video optimization for all devices

--- src/components/CommunityEngagement.vue
+++ src/components/CommunityEngagement.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="-mt-28 mb-10">
-    <div class="relative h-screen">
-      <video class="block h-full object-cover" src="/charity-vid-1.MP4" autoplay muted loop></video>
+    <div class="relative min-h-screen">
+      <video class="block min-h-screen object-cover" src="/charity-vid-1.MP4" autoplay muted loop></video>
       <div class="absolute inset-0 flex items-end bg-transparent">
         <h1 class="langar-regular ml-10 mb-10 text-yellow-500 text-5xl">CHARITY COMMUNITY</h1>
       </div>
