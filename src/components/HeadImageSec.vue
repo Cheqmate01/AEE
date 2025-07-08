3825b006d8a0738eb63fda70980156afce47421d
Cheqmate01 <cheqmater01@gmail.com>
2025-07-08 01:19:45 +0100

    head image optimization for all devices

--- src/components/HeadImageSec.vue
+++ src/components/HeadImageSec.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="-mt-28 mb-10 min-h-screen">
-    <img class="block h-full object-cover" src="/head-image.jpg" alt="">
+    <img class="block min-h-screen object-cover" src="/head-image.jpg" alt="">
   </div>
 </template>
